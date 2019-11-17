22ba7f709252e7d37b939c3f97ff60b837aef99f
Lairson, John Matthew <jomalair@iu.edu>
2019-11-17 18:58:12 -0500

    added pseudocode for models.py, and set up 'under construction' message on humorously/index

--- humorously/models.py
+++ humorously/models.py
@@ -1,3 +1,70 @@
 from django.db import models
 
 # Create your models here.
+
+# models that are currently in 'the jokeable' lucidchart
+# https://www.lucidchart.com/documents/edit/920b7f8e-419b-4316-b131-b79178fd8c96/
+
+"""
+a description of user.py
+=======================
+userid Number(10) (PK)
+lastname varchar(25)
+firstname varchar(25)
+emailaddress varchar(40)
+cityresidence char(10) (maybe make the limit higher here)
+state char(2) (maybe make the limit hold the whole state name?)
+(suggested:  zipcode varchar(25) (internationalization and zip+4))
+(suggested: country (varchar(50)))
+(suggested:  photoURL - this can be selected from a static list of assets in the app)
+"""
+
+"""
+category.py
+============================
+a description of category.py
+categoryid varchar(10) (PK)
+categoryname varchar(40)
+categorydescription varchar(255) (max length or something like that)
+"""
+
+"""
+joke.py
+============================
+jokeid number(10) (PK)
+userid number(10) (FK)
+categoryid varchar(10) (FK)
+datecreated (date)
+joketitle varchar(40)
+joketext varchar(350)
+"""
+
+"""
+review.py
+=============================
+reviewid Number(10) (PK)
+userid Number(10) (FK)
+jokeid Number(10) (FK)
+score Number(1)
+reviewtitle varchar(40)
+reviewtext varchar(255) (max length or something like that)
+"""
+
+"""
+set.py
+==============================
+setid Number(10) (PK)
+userid number(10) (FK)
+setname varchar(40)
+setdescription varchar(255) (max length or something like that)
+"""
+
+"""
+act.py
+===============================
+actid Number(10) (PK)
+userid Number(10) (FK)
+setid Number(10) (FK)
+actname varchar(40)
+actdescription varchar(255) (max length or something like that)
+"""
--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
+    path('humorously/', include('humorously.urls')),
     path('admin/', admin.site.urls),
 ]
