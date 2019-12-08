63204dfc608858a14e45c1d87323a1714b0717c5
Lairson, John Matthew <jomalair@iu.edu>
2019-12-08 12:07:18 -0500

    extended the user model to have 1:1 relationship with jokester, to use the framework's native auth pattern

--- humorously/models.py
+++ humorously/models.py
@@ -1,7 +1,11 @@
 import datetime
 
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -12,9 +16,11 @@ from django.utils import timezone
 # Django has a built in User Object - here is a link to the documentation
 # https://docs.djangoproject.com/en/2.2/topics/auth/default/
 class Jokester(models.Model):
-    last_name = models.CharField(max_length=150)
-    first_name = models.CharField(max_length=150)
-    email_address = models.CharField(max_length=150)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        default=1
+    )
     city_residence = models.CharField(max_length=150)
     state = models.CharField(max_length=150)
     zipcode = models.CharField(max_length=25)
@@ -23,7 +29,16 @@ class Jokester(models.Model):
         auto_now_add=True
     )
     def __str__(self):
-        return self.last_name + ", " + self.first_name
+        return self.user.last_name + ", " + self.user.first_name
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Jokester.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.jokester.save()
 
 """
 a description of user.py
@@ -39,6 +54,14 @@ state char(2) (maybe make the limit hold the whole state name?)
 (suggested:  photoURL - this can be selected from a static list of assets in the app)
 """
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=255)
+    created = models.DateTimeField(
+        auto_now_add=True #had problems with datetimes evaluating to a specific date and time on the migration side.  auto_now_add is a bit that django has included to tell the database we want a default timestamp
+    )
+    def __str__(self):
+        return self.name
 """
 category.py
 ============================
@@ -47,6 +70,22 @@ categoryid varchar(10) (PK)
 categoryname varchar(40)
 categorydescription varchar(255) (max length or something like that)
 """
+class Joke(models.Model):
+    user = models.ForeignKey(
+        User,#even though we don't specify a class for the user, this should relate back to the django.auth.user model that is included with the framework by default
+        on_delete=models.CASCADE,#this should delete any jokes a user creates if they are deleted
+        related_name='+', #setting this to '+' tells the framework that we don't want to relate the user back to the joke on the other side, we just want the id on this side.
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='+',
+    )
+    created = models.DateTimeField(
+        auto_now_add=True
+    )
+    title = models.CharField(max_length=50)
+    text = models.TextField(max_length=1024)
 
 """
 joke.py
@@ -58,7 +97,25 @@ datecreated (date)
 joketitle varchar(40)
 joketext varchar(350)
 """
-
+class Review(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='+',
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='+',
+    )
+    joke = models.ForeignKey(
+        Joke,
+        on_delete=models.CASCADE,
+        related_name='+',
+    )
+    score = models.PositiveSmallIntegerField()
+    title = models.CharField(max_length=50)
+    text = models.TextField(max_length=1024)
 """
 review.py
 =============================
@@ -69,7 +126,14 @@ score Number(1)
 reviewtitle varchar(40)
 reviewtext varchar(255) (max length or something like that)
 """
-
+class Set(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='+',
+    )
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=1024)
 """
 set.py
 ==============================
@@ -78,7 +142,17 @@ userid number(10) (FK)
 setname varchar(40)
 setdescription varchar(255) (max length or something like that)
 """
-
+class Act(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='+',
+    )
+    set = models.ForeignKey(
+        Set,
+        on_delete=models.CASCADE,
+        related_name='+',
+    )
 """
 act.py
 ===============================
