d35221c9c037abf44b69d4720990aae99a7ac2af
Angelina Cooper <angecoop@iupui.edu>
2019-12-16 19:01:14 -0500

    Added Deptree and screenshots of the deptree process

--- sql/dependencies/deptree_fill.sql
+++ sql/dependencies/deptree_fill.sql
@@ -1,3 +1,18 @@
 execute deptree_fill('table', 'system', 'humorously_joke');
-
 select * from deptree;
+execute deptree_fill('table', 'system', 'humorously_act');
+select * from deptree;
+execute deptree_fill('table', 'system', 'humorously_category');
+select * from deptree;
+execute deptree_fill('table', 'system', 'humorously_club'); 
+select * from deptree;
+execute deptree_fill('table', 'system', 'humorously_jokester'); 
+select * from deptree;
+execute deptree_fill('table', 'system', 'humorously_review');
+select * from deptree;
+execute deptree_fill('table', 'system', 'humorously_set'); 
+select * from deptree;
+ 
+ select *
+ From user_objects
+ where lower(object_name) like '%humorously%';
