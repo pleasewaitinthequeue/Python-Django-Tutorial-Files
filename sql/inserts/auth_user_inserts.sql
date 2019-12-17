ef11c697b5b3bec98ad7386db4bfbf39c5e3ea88
Lairson, John Matthew <jomalair@iu.edu>
2019-12-16 22:36:20 -0500

    added auth_user insert sql

---
+++ sql/inserts/auth_user_inserts.sql
@@ -0,0 +1,13 @@
+insert all
+into auth_user (ID, PASSWORD, LAST_LOGIN, IS_SUPERUSER, USERNAME, FIRST_NAME, LAST_NAME, EMAIL, IS_STAFF, IS_ACTIVE, DATE_JOINED )
+    values ( 1,'pbkdf2_sha256$150000$uvotM7FPxQ03$IaW7BAvHOmEw+SWe0vAfmbSWiKbIZ2yux+BgfRd0TQs=','15-DEC-19 06.01.29.873876000 PM',1,'admin','System','Administrator','jomalair@iu.edu',1,1,'08-DEC-19 02.34.45.000000000 AM' )
+into auth_user (ID, PASSWORD, LAST_LOGIN, IS_SUPERUSER, USERNAME, FIRST_NAME, LAST_NAME, EMAIL, IS_STAFF, IS_ACTIVE, DATE_JOINED )
+    values ( 2,'pbkdf2_sha256$150000$tkNLava6hZRf$dcYVd4uBe8MD5eW7N8TX6ibeaZ4uI8wi41A4llwcwYE=','08-DEC-19 02.36.15.000000000 AM',1,'django','Django','PowerUser','john.lairson@gmail.com',1,1,'08-DEC-19 02.35.07.000000000 AM' )
+into auth_user (ID, PASSWORD, LAST_LOGIN, IS_SUPERUSER, USERNAME, FIRST_NAME, LAST_NAME, EMAIL, IS_STAFF, IS_ACTIVE, DATE_JOINED )
+    values ( 44,'pbkdf2_sha256$150000$cL1RGtyQFQPn$EjxGpZ0+YBwUO9Bpf2WRcMy12GqqxgRqDLru8XAeSBk=','08-DEC-19 09.43.22.802070000 PM',0,'jokestimisprime','Jokefully','Jokester','jokefully.jokester@gmail.com',0,1,'08-DEC-19 05.01.14.000000000 PM')
+into auth_user (ID, PASSWORD, LAST_LOGIN, IS_SUPERUSER, USERNAME, FIRST_NAME, LAST_NAME, EMAIL, IS_STAFF, IS_ACTIVE, DATE_JOINED )
+    values ( 61,'pbkdf2_sha256$150000$qtsthueBhMTC$/2+bHEfjnRZcRhHRjDTfFKzIPEXyFcEGQxYECb1vyVk=',null,0,'jokecepticon','Sir','Jokesalot','sir.jokesalot@humor.org',0,1,'08-DEC-19 05.27.23.000000000 PM' )
+select 1 from dual;
+
+
+
