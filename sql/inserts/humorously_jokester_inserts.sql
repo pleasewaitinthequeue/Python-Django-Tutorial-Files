5c32146075dd065f51b6d06bbda7341889c7cc2a
Lairson, John Matthew <jomalair@iu.edu>
2019-12-16 22:15:32 -0500

    added jokester, review insert statements

---
+++ sql/inserts/humorously_jokester_inserts.sql
@@ -0,0 +1,21 @@
+insert all
+into humorously_jokester 
+    (ID, CITY_RESIDENCE, STATE, ZIPCODE, COUNTRY, CREATED, USER_ID, PHOTO_URL)
+values 
+    ( 1, 'Indianapolis','Indiana','46203','United States','08-DEC-19 04.53.19.854000000 PM',1,'https://lh4.googleusercontent.com/-QpKk-0G82AE/AAAAAAAAAAI/AAAAAAAAAAg/3r3-YNV9cew/photo.jpg' )
+into humorously_jokester 
+    (ID, CITY_RESIDENCE, STATE, ZIPCODE, COUNTRY, CREATED, USER_ID, PHOTO_URL)
+values 
+    ( 22,'Indianapolis','Indiana','46203','United States','08-DEC-19 05.01.14.606196000 PM',44,'https://lh4.googleusercontent.com/-QpKk-0G82AE/AAAAAAAAAAI/AAAAAAAAAAg/3r3-YNV9cew/photo.jpg' )
+into humorously_jokester 
+    (ID, CITY_RESIDENCE, STATE, ZIPCODE, COUNTRY, CREATED, USER_ID, PHOTO_URL)
+values
+    ( 41,'Indianapolis','Indiana','46203','United States','08-DEC-19 05.27.23.215520000 PM',61,'https://lh4.googleusercontent.com/-QpKk-0G82AE/AAAAAAAAAAI/AAAAAAAAAAg/3r3-YNV9cew/photo.jpg' )
+into humorously_jokester 
+    (ID, CITY_RESIDENCE, STATE, ZIPCODE, COUNTRY, CREATED, USER_ID, PHOTO_URL)
+values
+    ( 2,'Indianapolis','Indiana','46203','United States','08-DEC-19 04.54.01.883000000 PM',2,'https://lh4.googleusercontent.com/-QpKk-0G82AE/AAAAAAAAAAI/AAAAAAAAAAg/3r3-YNV9cew/photo.jpg' )
+select 1 from dual;
+
+
+
---
+++ sql/inserts/humorously_review_inserts.sql
@@ -0,0 +1,20 @@
+insert all
+
+into humorously_review ( ID, SCORE, TITLE, TEXT, JOKE_ID, USER_ID )
+    values ( 1,9,'An Oldie But a Goodie	This is a great traditional joke that will never get old.',1,1 )
+into humorously_review ( ID, SCORE, TITLE, TEXT, JOKE_ID, USER_ID )
+    values ( 21,3,'This Joke Stinks	Need to work on your material.',41,1 )
+into humorously_review ( ID, SCORE, TITLE, TEXT, JOKE_ID, USER_ID )
+    values ( 41,8,'Number One Dad Joke	This is such a dad joke.  I have never heard a better one about switzerland.',61,1 )
+into humorously_review ( ID, SCORE, TITLE, TEXT, JOKE_ID, USER_ID )
+    values ( 42,3,'I am From Switzerland	I am from Switzerland and this Joke does not amuse me.',61,1 )
+into humorously_review ( ID, SCORE, TITLE, TEXT, JOKE_ID, USER_ID )
+    values ( 61,9,'Switzerland is a Small Country	Switzerland is a small country and this joke is very very funny.  Hardy Har Har.',61,1 )
+into humorously_review ( ID, SCORE, TITLE, TEXT, JOKE_ID, USER_ID )
+    values ( 81,0,'This joke sucks	i don't like this joke one bit.',1,1 )
+select 1 from dual;
+
+
+
+
+
